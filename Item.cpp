8200ddf6b33f86f203f7ccbc4aabc801e21ae3e4
it21924750 <99161642+it21924750@users.noreply.github.com>
2022-04-18 16:34:00 +0000

    done now

--- Item.cpp
+++ Item.cpp
@@ -2,6 +2,20 @@
 #include <iostream>
 using namespace std;
 
+Item::Item()
+{
+  itemCode = 0;
+  unitPrice = 0;
+}
+Item::Item (int iCode,int uPrice)
+{
+  itemCode = iCode;
+  unitPrice = uPrice;
+}
+Item::~Item()
+{
+  cout<<"Destructor called"<<endl;
+}
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
