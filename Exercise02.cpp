aeb0fb07d1036d3827d07dbd76626221c27d3fda
it21924750 <99161642+it21924750@users.noreply.github.com>
2022-04-18 16:44:11 +0000

    done now1

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -14,14 +14,20 @@ int main() {
    cin >> radius;
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
-   // 1. Create a dynamic Rectangle type variable (pointer)
-   // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
-   // 3. Create a dynamic Circle type variable (pointer)
-   // 4. Create a dynamic Circle Object set radius that was input from the keyboard
-   // 5. Call the display method of the Rectangle Object
-   // 6. Call the display method of the Circle Object
-   // 7. Delete the Rectangle Object from memory
-   // 8. Delete the Circle Object from memory
+   Rectangle *r1;// 1. Create a dynamic Rectangle type variable (pointer)
+   r1 = new Rectangle(length,width);
+ // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+  Circle *c1;
+// 3. Create a dynamic Circle type variable (pointer)
+   c1 = new Circle(radius);
+// 4. Create a dynamic Circle Object set radius that was input from the keyboard
+   r1->display();
+// 5. Call the display method of the Rectangle Object
+   c1->display();
+// 6. Call the display method of the Circle Object
+   delete r1;// 7. Delete the Rectangle Object from memory
+   delete c1;
+// 8. Delete the Circle Object from memory
   
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
